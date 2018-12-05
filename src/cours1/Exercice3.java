a03256fd855ec069505c8699038442a2c2e4076e
Théo Huchard <exoth.ousha@gmail.com>
2018-12-05 22:29:16 +0100

    Update Exercice3.java

--- src/cours1/Exercice3.java
+++ src/cours1/Exercice3.java
@@ -1,8 +1,46 @@
 package cours1;
 
 public class Exercice3 {
+    private static boolean exceptionOne(String[] stringArray) {
+        for (String elem : stringArray) {
+            try {
+                Integer.parseInt(elem);
+            } catch (NumberFormatException e) {
+                throw new NumberFormatException();
+            }
+        }
+        return true;
+    }
 
-    public static void main(String[]args){
-        
+    private static boolean exceptionTwo(String[] stringArray) {
+        for (String elem : stringArray) {
+            try {
+                Integer.parseInt(elem);
+            } catch (NumberFormatException e) {
+                System.out.println(elem + " n'est pas un nombre");
+                System.out.println("L'exception provient de la méthode : " + e.getStackTrace()[e.getStackTrace().length - 2].getMethodName());
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean exceptionThree(String[] stringArray){
+        try{
+
+        }catch (){
+            
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] arr = {"156465", "16546534", "Bonjour"};
+
+        // Part 1
+        //exceptionOne(arr);
+        // Part 2
+        exceptionTwo(arr);
+
+
     }
 }
