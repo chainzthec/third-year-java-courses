5294051726ec9e675309d0a87478bd567da46d3d
Théo Huchard <exoth.ousha@gmail.com>
2018-12-05 21:22:12 +0100

    Finished Exercice2 init exercice3

--- src/cours1/Exercice2.java
+++ src/cours1/Exercice2.java
@@ -3,28 +3,46 @@ package cours1;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
 
 public class Exercice2 {
 
-    public void afficherFichier(String nomFichier) throws IOException {
+    private void afficherFichier(String nomFichier) throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader(nomFichier));
         String line = reader.readLine();
+        System.out.println("Liste des notes : \n");
         System.out.println(line);
-        while ((line = reader.readLine()) != null) {
+        while ((line = reader.readLine()) != null)
             System.out.println(line);
-        }
+        System.out.println();
     }
 
-    public void extractMailAndPointFromFile(String nomFichier) throws IOException{
+    private void extractMailAndPointFromFile(String nomFichier) throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader(nomFichier));
-        String line = reader.readLine();
-        while((line = reader.readLine()) != null){
+        ArrayList<String> scoreList = new ArrayList<>();
+        String line;
+        while ((line = reader.readLine()) != null)
+            scoreList.add(line.substring(line.indexOf(":") + 1));
+        System.out.println("La moyenne des notes est " + moyenneStringArrayList(scoreList));
+    }
 
-        }
+    private int moyenneStringArrayList(ArrayList<String> arrList) {
+        int total = 0;
+        for (String elem : arrList)
+            total += Integer.parseInt(elem);
+        return total / arrList.size();
     }
 
     public static void main(String[] args) throws IOException {
+        //Heure actuelle
+        Instant start = Instant.now() ;
         Exercice2 exDeux = new Exercice2();
         exDeux.afficherFichier("misc/test.txt");
+        exDeux.extractMailAndPointFromFile("misc/test.txt");
+        // calcul d'une durée
+        Duration duration = Duration.between(start, Instant.now()) ;
+        System.out.println("Le temps d'exécution était de " + duration);
     }
 }
---
+++ src/cours1/Exercice3.java
@@ -0,0 +1,8 @@
+package cours1;
+
+public class Exercice3 {
+
+    public static void main(String[]args){
+        
+    }
+}
