99c878b717a1eab984462c423226faa7bbe3c1d5
Théo Huchard <exoth.ousha@gmail.com>
2019-02-07 20:25:04 +0100

    Finish Exercice2 -> a) -> 1st point.

--- src/chapter2/exercices/exercice2/Application.java
+++ src/chapter2/exercices/exercice2/Application.java
@@ -1,8 +1,62 @@
 package chapter2.exercices.exercice2;
 
+import java.util.ArrayList;
+
 public class Application {
 
+    private ArrayList<ArrayList<Mark>> arrayList;
 
+    Application() {
+        this.arrayList = new ArrayList<>();
+        ArrayList<Mark> student1Marks = new ArrayList<>();
+        ArrayList<Mark> student2Marks = new ArrayList<>();
+        student1Marks.add(new Mark(20, "Mathematics Course"));
+        student1Marks.add(new Mark(18, "C Course"));
+        student2Marks.add(new Mark(16, "Mathematics Course"));
+        student2Marks.add(new Mark(13, "Mathematics Course"));
+        student2Marks.add(new Mark(18, "C Course"));
+        student2Marks.add(new Mark(17, "Java Course"));
+        student2Marks.add(new Mark(15, "Big Data Course"));
+        mergeMarks(student1Marks);
+        mergeMarks(student2Marks);
+        this.arrayList.add(student1Marks);
+        this.arrayList.add(student2Marks);
+    }
+
+    void print() {
+        int i = 1;
+        int j;
+        for (ArrayList<Mark> al : arrayList) {
+            j = 1;
+            System.out.println("Student " + i + " : ");
+            for (Mark mark : al) {
+                System.out.println("     Mark " + j + " subject : " + mark.getSubject());
+                System.out.println("     Mark " + j + " value : " + mark.getValue());
+                j++;
+            }
+            i++;
+        }
+    }
+
+    private void mergeMarks(ArrayList<Mark> marks) {
+        float value;
+        ArrayList<Mark> marksToRemove = new ArrayList<>();
+        for (Mark mark1 : marks) {
+            if(!marksToRemove.contains(mark1)) {
+                for (Mark mark2 : marks) {
+                    if (!mark1.equals(mark2) && mark1.getSubject().equals(mark2.getSubject())) {
+                        value = (mark1.getValue() + mark2.getValue()) / 2;
+                        mark1.setValue(value);
+                        marksToRemove.add(mark2);
+                    }
+                }
+            }
+        }
+        //To avoid concurrency
+        for (Mark markToRemove : marksToRemove) {
+            marks.remove(markToRemove);
+        }
+    }
 
     @Override
     protected Object clone() throws CloneNotSupportedException {
--- src/tools/ExecutionTime.java
+++ src/tools/ExecutionTime.java
@@ -6,13 +6,12 @@ import java.time.Instant;
 public class ExecutionTime {
 
     private static Instant start;
-    private static Duration duration;
 
     public static void start(){
         start = Instant.now() ;
     }
     public static void end(){
-        duration = Duration.between(start, Instant.now()) ;
+        Duration duration = Duration.between(start, Instant.now());
         System.out.println("Process finished with execution time of: " + duration.toString().split("PT")[1].split("S")[0] + " seconds.");
     }
 }
