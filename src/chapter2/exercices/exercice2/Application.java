469396ac908c4a113daae5b00e00ebf75e3238d3
Théo Huchard <theo.huchard@gmail.com>
2019-02-19 21:54:21 +0100

    Finished Exercice2

--- src/chapter2/exercices/exercice2/Application.java
+++ src/chapter2/exercices/exercice2/Application.java
@@ -1,6 +1,8 @@
 package chapter2.exercices.exercice2;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 
 public class Application {
 
@@ -42,7 +44,7 @@ public class Application {
         float value;
         ArrayList<Mark> marksToRemove = new ArrayList<>();
         for (Mark mark1 : marks) {
-            if(!marksToRemove.contains(mark1)) {
+            if (!marksToRemove.contains(mark1)) {
                 for (Mark mark2 : marks) {
                     if (!mark1.equals(mark2) && mark1.getSubject().equals(mark2.getSubject())) {
                         value = (mark1.getValue() + mark2.getValue()) / 2;
@@ -71,5 +73,61 @@ public class Application {
     @Override
     public String toString() {
         return super.toString();
+    }
+
+    public float average(int i) {
+        float total = 0;
+        int totalCoefficient = 0;
+        ArrayList<Mark> al = arrayList.get(i);
+        for (Mark el : al) {
+            total += el.getValue();
+            totalCoefficient++;
+        }
+        return total / totalCoefficient;
+    }
+
+    public void add(int i, float v, String c) {
+        arrayList.get(i).add(new Mark(v, c));
+    }
+
+    public int compute() {
+        int maxSize = 0;
+        for (int i = 0; i < arrayList.size(); i++)
+            if (maxSize < arrayList.get(i).size())
+                maxSize = arrayList.get(i).size();
+        return maxSize;
+    }
+
+    public float[] avgs() {
+        float[] marksArray = new float[arrayList.size()];
+        int i = 0;
+        for (ArrayList<Mark> marks : arrayList) {
+            marksArray[i] = average(i);
+            i++;
+        }
+        Arrays.sort(marksArray);
+        return marksArray;
+    }
+
+    public void showAvgs(float[] avgs){
+        for(int i = 0; i < avgs.length; i ++){
+            System.out.print(avgs[i]);
+            if(i != avgs.length)
+                System.out.print(" ");
+            else{
+                System.out.println();
+            }
+        }
+    }
+
+    public void testHashSet() {
+        HashSet hs = new HashSet();
+        System.out.println(arrayList.get(0).hashCode());
+        System.out.println(arrayList.get(1).hashCode());
+        hs.add(arrayList.get(0).get(0));
+        hs.add(arrayList.get(1).get(1));
+        for(Object obj : hs){
+            System.out.println("Class of the object is :" + obj.getClass());
+        }
     }
 }
